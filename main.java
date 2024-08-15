a46b1c9c9977badb006b18f53e90c8d84ab35fa6
Technology Rooms <technologyrooms@colsanjose.edu.co>
2024-08-15 14:37:55 -0500

    mi primer programita en Java B)

---
+++ main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String args[]){
+        int x = 1000;
+        float y = 0.5f;
+        double z = 800;
+        boolean q = true;
+        System.out.print("hola papi lindo");
+    }
+}
